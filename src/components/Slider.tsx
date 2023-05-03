eb117ea90dbde37021f87c0043bf1640df8b2449
NataliyaChe <happylibrarygeek@gmail.com>
2023-05-03 14:58:18 +0300

    slider fix

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -16,24 +16,24 @@ export function Slider({ newCourses }: SliderProps) {
                 autoPlay={false}
                 navButtonsAlwaysInvisible={true}>
             {newCourses.map(item => (
-                <Card variant="elevation" elevation={0} sx={{bgcolor: 'transparent', width: 215, pt: '33px', ml: '353px'}}>
+            //     <Card variant="elevation" elevation={0} sx={{bgcolor: 'transparent', width: 215, pt: '33px', ml: '353px'}}>
                 
-                <CardContent sx={{p: 0, mb: '16px'}}>
-                    <Typography variant='h3' sx={{color: '#FFFFFF', lineHeight: '32px', mb: '8px'}}>{item.title}</Typography>
-                    <Typography variant='caption' >
-                        {item.text}
-                    </Typography>
-                </CardContent>
-                <CardActions sx={{p: 0, mb: '42px'}}>
-                    <Button variant="contained" sx={{width: '158px', bgcolor: '#FFFFFF', borderRadius: '16px', py: '18px'}} >
-                        <Typography variant='caption' sx={{color: '#11142D', fontWeight: 700, textTransform: 'none'}}>
-                            Learn now
-                        </Typography>
-                    </Button>
-                </CardActions>
-            </Card>
+            //     <CardContent sx={{p: 0, mb: '16px'}}>
+            //         <Typography variant='h3' sx={{color: '#FFFFFF', lineHeight: '32px', mb: '8px'}}>{item.title}</Typography>
+            //         <Typography variant='caption' >
+            //             {item.text}
+            //         </Typography>
+            //     </CardContent>
+            //     <CardActions sx={{p: 0, mb: '42px'}}>
+            //         <Button variant="contained" sx={{width: '158px', bgcolor: '#FFFFFF', borderRadius: '16px', py: '18px'}} >
+            //             <Typography variant='caption' sx={{color: '#11142D', fontWeight: 700, textTransform: 'none'}}>
+            //                 Learn now
+            //             </Typography>
+            //         </Button>
+            //     </CardActions>
+            // </Card>
         //   <div key={item.id}>
-            // <HeroCard item={item} key={item.id}/>
+            <HeroCard item={item} key={item.id}/>
         //   </div>
         ))}
         </Carousel>
