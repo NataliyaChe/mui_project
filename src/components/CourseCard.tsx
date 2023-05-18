58db2393a3d17345fba9645e744ae62dae0c967b
NataliyaChe <happylibrarygeek@gmail.com>
2023-05-18 21:00:13 +0300

    CourseCard style fixed

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -25,17 +25,33 @@ export function CourseCard({courseItem}: CourseCardProps) {
     const id = open ? 'simple-popover' : undefined;
 
     return (
-        <Card variant="outlined" sx={{width: '190px', borderRadius: '24px', px: 0}}>
-            <CardActionArea sx={{pl: '4px', pt: '8px', mb: 0}}>
-                <CardContent sx={{ pb: 0}}>
-                    <Typography variant='h6' sx={{mb: '8px', lineHeight: '19.36px'}}>{courseItem.title}</Typography>
-                    <Typography variant='body2' sx={{mb: '37px', width: '60%'}}>{courseItem.text}</Typography>
+        <Card variant="outlined" 
+        sx={{width: '190px', borderRadius: '24px', p: '24px 15px 6px 20px'}}>
+            <CardActionArea>
+                <CardContent 
+                sx={{ p: 0}}
+                >
+                    <Typography variant='h6' 
+                    sx={{mb: '8px', lineHeight: '19.36px'}}>
+                        {courseItem.title}
+                    </Typography>
+                    <Typography variant='body2' 
+                    sx={{mb: '37px', width: '60%'}}>
+                        {courseItem.text}
+                    </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions sx={{pl: '20px', display: 'block', pt: 0}}>
-                    <Typography variant='body1' component='h3' sx={{mb: '16px', }}>{courseItem.subscribersQty}</Typography>
+            <CardActions 
+            sx={{display: 'block', p: 0}}>
+                    <Typography variant='body1' component='h3' 
+                    // sx={{mb: '16px', }}
+                    >
+                        {courseItem.subscribersQty}
+                    </Typography>
                 
-                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <Box 
+                sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}
+                >
                     <AvatarGroup max={6} spacing={'small'}>
                         <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 24, height: 24}}/>
                         <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" sx={{ width: 24, height: 24}}/>
@@ -44,8 +60,10 @@ export function CourseCard({courseItem}: CourseCardProps) {
                         <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" sx={{ width: 24, height: 24}}/>
                         <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" sx={{ width: 24, height: 24}}/>
                     </AvatarGroup>
-                    <Box sx={{mr: '15px'}}>
-                        <Checkbox icon={<StarOutlineRoundedIcon />} checkedIcon={<StarRoundedIcon />} defaultChecked sx={{width: 16, height: 16, mr: '6px', '&.Mui-checked': {color: '#F2C94C'}}}/>
+                    <Box>
+                        <Checkbox icon={<StarOutlineRoundedIcon />} checkedIcon={<StarRoundedIcon />} defaultChecked 
+                        sx={{width: 16, height: 16, mr: '6px', '&.Mui-checked': {color: '#F2C94C'}}}
+                        />
                         <IconButton onClick={handleClick} aria-describedby={id} sx={{width: 16}}>
                             <SvgIcon component={Dots} sx={{fill: 'none', width: '10px'}} inheritViewBox />
                         </IconButton>
